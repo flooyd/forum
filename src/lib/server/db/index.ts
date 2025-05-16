c19adc2d178c72b05deca9f9aa12040c0ecfcf99
Floyd Jones <floydtjones@gmail.com>
2025-05-16 01:05:47 -0700

    trigger redeploy

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,6 +3,7 @@ import postgres from 'postgres';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
+if(!env.SECRET) throw new Error('SECRET is not set');
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 const client = postgres(env.DATABASE_URL);
